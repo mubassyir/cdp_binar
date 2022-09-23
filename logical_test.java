a2f4b4647e5824b36460e6e2174fbd057ac1aa84
Ahmad Mubassyir <53318127+mubassyir@users.noreply.github.com>
2022-09-23 17:28:35 +0700

    Create logical_test.java
    Creating logical test

---
+++ logical_test.java
@@ -0,0 +1,37 @@
+class Result {
+
+    /*
+     * Complete the 'fizzBuzz' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void fizzBuzz(int n) {
+        for (int i = 1; i <= n; i++){
+       // System.out.println (i);
+        if (i%3==0 && i%5==0){
+            System.out.println("FizzBuzz");
+        } else if(i%3==0 && i%5!=0) {
+            System.out.println("Fizz");
+        } else if (i%3!=0 && i%5==0){
+            System.out.println("Buzz");
+        } else if(i%3!=0 && i%5!=0){
+            System.out.println(i);
+        }
+
+    }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.fizzBuzz(n);
+
+        bufferedReader.close();
+    }
+}
